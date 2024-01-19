2cf0aa598127b52fc69a30a670902ec78fd8eb53
gayo3n <ktq2390310@stu.o-hara.ac.jp>
2024-01-19 09:53:36 +0900

    Commit

--- exercise/chapter11/En111.java
+++ exercise/chapter11/En111.java
@@ -3,11 +3,38 @@ import java.util.Scanner;
 import practice.exercise.chapter11.Circle;
 
 public class En111 {
+    class Circle{
+        // フィールドを宣言
+        // 円周率
+        double pi;
+        // 半径
+        int r;
+        // 長さ
+        double length;
+        // 面積
+        double area;
+        // 円周の計算をするメソッド作成
+        public double math(int r, double pi){
+            // lengthに円周の計算結果を代入
+            length = (r + r) * pi;
+            // lengthを返す
+            return length;
+        }
+        // 面積の計算をするメソッド作成
+        public double aarea(int r, double pi){
+            // areaに面積の計算結果を代入
+            area = (r * r) * pi;
+            // areaを返す
+            return area;
+        }
+    }
     public static void main(String[] args){
+        // En111クラスのインスタンス化
+        En111 en111 = new En111();
+        // Circleクラスのインスタンス化
+        Circle circle = en111.new Circle();
         // Scannerクラスのインスタンス化
         Scanner stdIn = new Scanner(System.in);
-        // Circleクラスのインスタンス化
-        Circle circle = new Circle();
         // オブジェクトを生成
         circle.pi = 3.1415;
         // 半径の入力
@@ -18,8 +45,6 @@ public class En111 {
         // 円周
         System.out.println("円周の長さは" + circle.math(circle.r, circle.pi) + "です。");
         // 面積
-        System.out.println("円の面積は" + circle.aarea(circle.r, circle.pi) + "です。");
-        
+        System.out.println("円の面積は" + circle.aarea(circle.r, circle.pi) + "です。");   
     }
-    
 }
--- exercise/chapter11/En112.java
+++ exercise/chapter11/En112.java
@@ -1,9 +1,42 @@
 package practice.exercise.chapter11;
-
+import java.util.Scanner;
 public class En112 {
-    
+
+
 
     public static void main(String[] args){
+        // En112クラスのインスタンス化
+        En112 en112 = new En112();
+        // Mathクラスのインスタンス化
+        Math math = en112.new Math();
+        //  Scannerのインスタンス化
+        Scanner stdIn = new Scanner(System.in);
+        // 数値の入力
 
+        System.out.print("小さいほうの数値を入力してください：");
+        math.x = stdIn.nextInt();
+
+        System.out.print("大きいほうの数値を入力してください：");
+        math.y = stdIn.nextInt();
+
+        
+
+        System.out.println(math.x + "から" + math.y + "までの合計値は" + math.sum(math.x, math.y) + "です。");
+    }
+
+
+    class Math{
+        // インスタンス変数ｘ
+        public int x;
+        // インスタンス変数ｙ
+        public int y;
+        // 合計値の変数
+        public int sum;
+        public int sum(int a, int b){
+            for (int i = 0; b >= a; b--){
+                sum = sum +  b;
+            }
+        return sum;
+        }
     }
 }
